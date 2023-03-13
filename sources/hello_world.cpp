e0cbb840308bc0339406881c19a63430a9580568
PNuikin <p.nuikin@gmail.com>
2023-03-13 09:08:45 +0300

    fixed hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 int main()
 {
-  cout << "Hello, world!";
+  std::string name;
+  std::cout << "Enter your name:";
+  getline(std::cin, name);
+  std::cout << "Hello world from " << name;
   return 0;
 }
